5b1aa98fb1980efeb8829e891f81ceb95dfd672d
Hendi Santika <hendisantika@gmail.com>
2019-01-22 03:31:54 +0700

    #3 - Add lombok libs & Product Entity

---
+++ src/main/java/com/hendisantika/orderlock/entity/Product.java
@@ -0,0 +1,35 @@
+package com.hendisantika.orderlock.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : order-lock
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-01-22
+ * Time: 03:30
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Version
+    private Long version;
+
+    private String name;
+
+    private Long upc;
+
+    public Product(String name, Long upc) {
+        this.name = name;
+        this.upc = upc;
+    }
+}
