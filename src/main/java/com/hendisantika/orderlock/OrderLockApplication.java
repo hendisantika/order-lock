7630b9daf31ef964cfff9eb56d19b4aead7140da
Hendi Santika <hendisantika@gmail.com>
2019-01-22 03:27:41 +0700

    #1 - first commit

---
+++ src/main/java/com/hendisantika/orderlock/OrderLockApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.orderlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderLockApplication.class, args);
+    }
+
+}
+
